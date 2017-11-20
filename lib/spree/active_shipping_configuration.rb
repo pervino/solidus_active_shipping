3741447207acbf386a5d21bc5303a449e0bbde9f
ferBaz <bazfer@gmail.com>
2017-11-20 14:48:33 -0600

    using env vars for config

--- lib/spree/active_shipping_configuration.rb
+++ lib/spree/active_shipping_configuration.rb
@@ -1,19 +1,35 @@
 class Spree::ActiveShippingConfiguration < Spree::Preferences::Configuration
-  p = Spree::Preference
+  # p = Spree::Preference
 
-  preference :ups_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/ups_login').value
-  preference :ups_password, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/ups_password').value
-  preference :ups_key, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/ups_key').value
-  preference :shipper_number, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/shipper_number').value
+  # preference :ups_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/ups_login').value
+  # preference :ups_password, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/ups_password').value
+  # preference :ups_key, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/ups_key').value
+  # preference :shipper_number, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/shipper_number').value
 
-  preference :fedex_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_login').value
-  preference :fedex_password, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_password').value
-  preference :fedex_account, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_account').value
-  preference :fedex_key, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_key').value
+  # preference :fedex_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_login').value
+  # preference :fedex_password, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_password').value
+  # preference :fedex_account, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_account').value
+  # preference :fedex_key, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/fedex_key').value
 
-  preference :usps_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/usps_login').value
+  # preference :usps_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/usps_login').value
 
-  preference :canada_post_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/canada_post_login').value
+  # preference :canada_post_login, :string, :default => p.find_by(key: 'spree/active_shipping_configuration/canada_post_login').value
+
+  # -----------------------
+
+  preference :ups_login, :string, :default => ENV['UPS_LOGIN']
+  preference :ups_password, :string, :default => ENV['UPS_PASSWORD']
+  preference :ups_key, :string, :default => ENV['UPS_KEY']
+  preference :shipper_number, :string, :default => ENV['SHIPPER_NUMBER']
+
+  preference :fedex_login, :string, :default => ENV['FEDEX_LOGIN']
+  preference :fedex_password, :string, :default => ENV['FEDEX_PASSWORD']
+  preference :fedex_account, :string, :default => ENV['FEDEX_ACCOUNT']
+  preference :fedex_key, :string, :default => ENV['FEDEX_KEY']
+
+  preference :usps_login, :string, :default => ENV['USPS_LOGIN']
+
+  preference :canada_post_login, :string, :default => ENV['CANADA_POST_LOGIN']
 
   # The default values correspond to the official test credentials
   # Source : https://www.canadapost.ca/cpo/mc/business/productsservices/developers/services/fundamentals.jsf
