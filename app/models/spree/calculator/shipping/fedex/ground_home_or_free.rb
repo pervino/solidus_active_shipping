e38b8a510daa3d038109cc6aae9f843645e0ec9f
keithbooher <keith.booher@yahoo.com>
2020-04-23 17:37:05 -0500

    fix class name

--- app/models/spree/calculator/shipping/fedex/ground_home_or_free.rb
+++ app/models/spree/calculator/shipping/fedex/ground_home_or_free.rb
@@ -3,7 +3,7 @@ require_dependency 'spree/calculator'
 module Spree
   module Calculator::Shipping
     module Fedex
-      class GroundHomeDelivery < Spree::Calculator::Shipping::Fedex::Base
+      class GroundHomeOrFree < Spree::Calculator::Shipping::Fedex::Base
         def self.description
           I18n.t("fedex.ground_home_delivery")
         end
